3f7c567f63a7532d728aa1bc5b881e3c66f1ad39
Miguel <miguelquebsilva@hotmail.com>
2022-03-07 18:00:51 -0600

    primer commit

---
+++ base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, blank=True)
+    update = models.DateTimeField(auto_now=True)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
