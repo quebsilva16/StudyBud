38666833c3e0ea1a52ded4368046b6beb4b57903
Miguel <miguelquebsilva@hotmail.com>
2022-03-07 18:21:43 -0600

    primer committ

--- base/views.py
+++ base/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {"id": 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', )
 
 
 def room(request):
