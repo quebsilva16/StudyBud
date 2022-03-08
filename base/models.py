aa20fa51b5e2d79f11d2a60d2ad904cc85cdfeb6
Miguel <miguelquebsilva@hotmail.com>
2022-03-08 17:22:36 -0600

    08 de marzo

--- base/models.py
+++ base/models.py
@@ -1,13 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
-class Room(models.Model):
+class Topic(models.Model):
     name = models.CharField(max_length=200)
-    descripcion = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
     update = models.DateTimeField(auto_now=True)
     create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,17 +1,29 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Room
+from .forms import RoomForm
+
 
 # Create your views here.
-rooms = [
+'''rooms = [ 
     {'id': 1, 'name': 'Lets learn python!'},
     {"id": 2, 'name': 'Design with me'},
     {'id': 3, 'name': 'Frontend developers'},
-]
+]'''
 
 
 def home(request):
-    return render(request, 'home.html', )
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
